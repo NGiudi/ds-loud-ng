54fffdd6df6da2686866433981014eed4a0a7913
Nicolás <nicolas.m.giudice@gmail.com>
2023-09-18 00:40:27 -0300

    delete molecules

--- src/components/molecules/RulesPanel/utils/validationSchema.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const carSchema = Yup.object().shape({
-  marca: Yup.string().required("Campo requerido"),
-  modelo: Yup.string().required("Campo requerido"),
-  patente: Yup.string().required("Campo requerido"),
-});
