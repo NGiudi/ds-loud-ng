cea727861235461ae05c0cbe391664ed29d1ec70
Nicolás <nicolas.m.giudice@gmail.com>
2023-12-08 16:41:14 -0300

    update libraries versions

--- src/index.js
+++ src/index.js
@@ -2,50 +2,14 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
-/*******************************************************************************/
-/**                                                                           **/
-/** TODO LIST:                                                                **/
-/**   1. Revisar el componente de dropdown, tengo que hacer mejoras.          **/
-/**                                                                           **/
-/*******************************************************************************/
-
 //? fontawensome icons.
 library.add(fas, far);
 
 //? layout components
-export { Box, Columns, Flex, ThemeDS } from "./components/layout";
+export * from "./components/layout";
 
 //? atoms components
-export {
-  Avatar,
-  Badge,
-  Button,
-  Checkbox,
-  Countdown,
-  Divider,
-  Dropdown,
-  DropdownItem,
-  DropdownPanel,
-  DropdownToogle,
-  Icon,
-  IconButton,
-  Image,
-  Input,
-  Modal,
-  Panel,
-  ProgressBar,
-  Select,
-  SelectOption,
-  Table,
-  Tab,
-  Tabs,
-  Text,
-} from "./components/atoms";
+export * from "./components/atoms";
 
 //? molecules components
-export {
-  AppLayout,
-  ImageGroup,
-  Pagination,
-  StepsBar,
-} from "./components/molecules";
+export * from "./components/molecules";
