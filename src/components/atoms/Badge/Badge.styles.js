a00969f238c52eccfd7fc4e402b8f66974f90cd5
NGiudiDev <ngiudice.dev@gmail.com>
2025-03-25 22:12:08 -0300

    organizar codigo

--- src/components/atoms/Badge/Badge.styles.js
+++ src/components/atoms/Badge/Badge.styles.js
@@ -8,9 +8,9 @@ import {
 
 const Wrapper = styled("div")`
   ${(props) => css`
-    display: inline-block;
     background-color: ${getColor(props.theme, props.$color)};
     border-radius: 4px;
+    display: inline-block;
 
     ${marginProperties(props)}
     ${paddingProperties(props, "x-8 y-4")}
