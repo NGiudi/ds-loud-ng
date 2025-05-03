7b3860af282c1acc68a0e84246e6cb0da78edfa8
NGiudiDev <ngiudice.dev@gmail.com>
2025-05-03 18:17:49 -0300

    Updates

---
+++ assets/Checkbox.stories-CRLk5szA.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{U as h,e as f,f as g}from"./index-_wcBtDAo.js";import{T as n,b as a,C as r,F as p,a as b,B as y}from"./index-CscVSUHB.js";import"./iframe-CG8MbMVq.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const x=()=>e.jsx(h,{children:e.jsxs(n,{children:[e.jsx(a,{margin:"b-16",type:"pageTitle",children:"Checkbox"}),e.jsx(a,{children:"Los componentes checkbox son elementos fundamentales, diseñados para ofrecer una experiencia de usuario intuitiva y accesible en aplicaciones y sitios web. Los checkboxes permiten a los usuarios realizar selecciones binarias, lo que significa que pueden elegir entre dos estados claramente definidos, como marcado o desmarcado."}),e.jsx(f,{}),e.jsx(a,{margin:"b-16",type:"title",children:"Características"}),e.jsxs(a,{margin:"b-16",children:[e.jsxs(a,{as:"span",weight:"semibold",children:["Etiqueta Descriptiva:"," "]}),"Cada checkbox generalmente se asocia con una etiqueta descriptiva que indica su propósito o la opción que representa. Esta etiqueta aumenta la comprensión del usuario."]}),e.jsxs(a,{margin:"b-40",children:[e.jsxs(a,{as:"span",weight:"semibold",children:["Interacción Visual:"," "]}),"Los checkboxes suelen representarse como una pequeña casilla que puede estar marcada (con una marca de verificación) o desmarcada (sin marca de verificación). Los usuarios pueden cambiar su estado haciendo clic o tocando la casilla."]}),e.jsx(a,{type:"title",children:"Propiedades"}),e.jsx(g,{})]})});x.__docgenInfo={description:"",methods:[],displayName:"CheckboxDocs"};const T={argTypes:{description:{control:{type:"text"},description:"Texto más detallado que describe la funcionalidad y contiene otras observaciones relevantes."},id:{control:{type:"text"},description:"Identificador único para el componente"},label:{control:{type:"text"},description:"Un breve texto que describe la funcionalidad del checkbox",table:{defaultValue:{summary:"-"}}},margin:{control:{type:"text"},description:"Establece la distancia de los márgenes"},name:{control:{type:"text"},description:"Nombre identificador del componente para su uso con Formik",table:{defaultValue:{summary:"-"}}}},component:r,parameters:{docs:{page:x},status:{type:"stable"}},tags:["autodocs"],title:"Components/Checkbox"},s=o=>e.jsx(n,{children:e.jsx(p,{initialValues:{email:!1},children:e.jsx(b,{children:e.jsx(r,{...o})})})});s.args={description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",id:"story-checkbox",label:"Lorem ipsum",margin:"a-16",name:"email"};const i=()=>e.jsx(n,{children:e.jsx("div",{style:{maxWidth:"500px"},children:e.jsx(p,{initialValues:{email:!1,recommendations:!0},onSubmit:o=>{alert(JSON.stringify(o))},children:e.jsxs(b,{children:[e.jsx(r,{description:"Mantente al tanto de las últimas noticias, ofertas y actualizaciones de nuestra marca al marcar esta casilla. Recibirás correos electrónicos periódicos con información relevante sobre nuestros productos y servicios.",id:"checkbox-form-1",label:"Suscribirse a boletines informativos",margin:"b-24",name:"email"}),e.jsx(r,{description:"Al marcar esta casilla, nos das permiso para analizar tus compras anteriores y preferencias de navegación. De esta forma, podremos ofrecerte sugerencias de productos y servicios que se adapten a tus gustos y necesidades.",disabled:!0,id:"checkbox-form-2",label:"Recibe recomendaciones personalizadas",name:"recommendations"}),e.jsx(y,{margin:"t-20",type:"submit",children:"Submit"})]})})})});s.__docgenInfo={description:"",methods:[],displayName:"Model"};i.__docgenInfo={description:"",methods:[],displayName:"FormExample"};var t,c,l;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`args => {
+  return <ThemeDS>\r
+      <Formik initialValues={{
+      email: false
+    }}>\r
+        <Form>\r
+          <Checkbox {...args} />\r
+        </Form>\r
+      </Formik>\r
+    </ThemeDS>;
+}`,...(l=(c=s.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,m,u;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <div style={{
+      maxWidth: "500px"
+    }}>\r
+        <Formik initialValues={{
+        email: false,
+        recommendations: true
+      }} onSubmit={values => {
+        alert(JSON.stringify(values));
+      }}>\r
+          <Form>\r
+            <Checkbox description="Mantente al tanto de las últimas noticias, ofertas y actualizaciones de nuestra marca al marcar esta casilla. Recibirás correos electrónicos periódicos con información relevante sobre nuestros productos y servicios." id="checkbox-form-1" label="Suscribirse a boletines informativos" margin="b-24" name="email" />\r
+\r
+            <Checkbox description="Al marcar esta casilla, nos das permiso para analizar tus compras anteriores y preferencias de navegación. De esta forma, podremos ofrecerte sugerencias de productos y servicios que se adapten a tus gustos y necesidades." disabled id="checkbox-form-2" label="Recibe recomendaciones personalizadas" name="recommendations" />\r
+\r
+            <Button margin="t-20" type="submit">\r
+              Submit\r
+            </Button>\r
+          </Form>\r
+        </Formik>\r
+      </div>\r
+    </ThemeDS>;
+}`,...(u=(m=i.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const E=["Model","FormExample"];export{i as FormExample,s as Model,E as __namedExportsOrder,T as default};
---
+++ assets/Panel.stories-Inm9wGg7.js
@@ -0,0 +1,70 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{P as n,T as l,b as s}from"./index-CscVSUHB.js";import{f as c}from"./index-D_Ss_HUe.js";const b={argTypes:{align:{control:{type:"none"},description:"Recibe un string para alinear el contenido dentro del panel."},children:{control:{type:"none"},description:"Contenido del panel"},margin:{control:{type:"text"},description:"Se setea la distancia de los margenes."},onSizeH:{control:{type:"none"},description:"Función que se ejecuta al modificar el alto del panel."},padding:{control:{type:"text"},description:"Se setea la distancia de los paddings."},type:{control:{type:"select"},description:"Cambia el color de fondo y del texto.",options:["error","info","neutral","success","warning"]}},component:n,parameters:{status:{type:"beta"}},tags:["autodocs"],title:"Components/Panel"},t=u=>e.jsx(l,{children:e.jsx(n,{...u})});t.args={children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",onSizeH:c(),type:"neutral"};const r=()=>e.jsxs(l,{children:[e.jsx(n,{margin:"b-12",children:e.jsx(s,{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including Lorem Ipsum."})}),e.jsx(n,{margin:"b-12",type:"error",children:e.jsx(s,{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."})}),e.jsx(n,{margin:"b-12",type:"info",children:e.jsx(s,{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."})}),e.jsx(n,{margin:"b-12",type:"success",children:e.jsx(s,{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only centuries, but also the leap into electronic typesetting, remaining remaining remaining essentially unchanged. It was popularised in the the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."})}),e.jsx(n,{margin:"b-12",type:"warning",children:e.jsx(s,{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."})})]});t.__docgenInfo={description:"",methods:[],displayName:"Model"};r.__docgenInfo={description:"",methods:[],displayName:"Types"};var i,a,o;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`args => {
+  return <ThemeDS>\r
+      <Panel {...args} />\r
+    </ThemeDS>;
+}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var p,m,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <Panel margin="b-12">\r
+        <Text>\r
+          Lorem Ipsum is simply dummy text of the printing and typesetting\r
+          industry. Lorem Ipsum has been the industry standard dummy text ever\r
+          since the 1500s, when an unknown printer took a galley of type and\r
+          scrambled it to make a type specimen book. It has survived not only\r
+          centuries, but also the leap into electronic typesetting, remaining\r
+          essentially unchanged. It was popularised in the 1960s with the\r
+          Letraset sheets containing Lorem Ipsum passages, and more recently\r
+          with desktop publishing software like Aldus PageMaker including Lorem\r
+          Ipsum.\r
+        </Text>\r
+      </Panel>\r
+\r
+      <Panel margin="b-12" type="error">\r
+        <Text>\r
+          Lorem Ipsum is simply dummy text of the printing and typesetting\r
+          industry. Lorem Ipsum has been the industry standard dummy text ever\r
+          since the 1500s, when an unknown printer took a galley of type and\r
+          scrambled it to make a type specimen book. It has survived not only\r
+          centuries, but also the leap into electronic typesetting, remaining\r
+          essentially unchanged. It was popularised in the 1960s with the of\r
+          Letraset sheets containing Lorem Ipsum passages, and more recently\r
+          with desktop publishing software like Aldus PageMaker including\r
+          versions of Lorem Ipsum.\r
+        </Text>\r
+      </Panel>\r
+\r
+      <Panel margin="b-12" type="info">\r
+        <Text>\r
+          Lorem Ipsum is simply dummy text of the printing and typesetting\r
+          industry.\r
+        </Text>\r
+      </Panel>\r
+\r
+      <Panel margin="b-12" type="success">\r
+        <Text>\r
+          Lorem Ipsum is simply dummy text of the printing and typesetting\r
+          industry. Lorem Ipsum has been the industry standard dummy text ever\r
+          since the 1500s, when an unknown printer took a galley of type and\r
+          scrambled it to make a type specimen book. It has survived not only\r
+          centuries, but also the leap into electronic typesetting, remaining\r
+          remaining remaining essentially unchanged. It was popularised in the\r
+          the release of Letraset sheets containing Lorem Ipsum passages, and\r
+          more recently with desktop publishing software like Aldus PageMaker\r
+          including versions of Lorem Ipsum.\r
+        </Text>\r
+      </Panel>\r
+\r
+      <Panel margin="b-12" type="warning">\r
+        <Text>\r
+          Lorem Ipsum is simply dummy text of the printing and typesetting\r
+          industry. Lorem Ipsum has been the industry standard dummy text ever\r
+          since the 1500s, when an unknown printer took a galley of type and\r
+          scrambled it to make a type specimen book. It has survived not only\r
+          five centuries, but also the leap into electronic typesetting,\r
+          remaining essentially unchanged. It was popularised in the 1960s with\r
+          the release of Letraset sheets containing Lorem Ipsum passages, and\r
+          more recently with desktop publishing software like Aldus PageMaker\r
+          including versions of Lorem Ipsum.\r
+        </Text>\r
+      </Panel>\r
+    </ThemeDS>;
+}`,...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const f=["Model","Types"];export{t as Model,r as Types,f as __namedExportsOrder,b as default};
