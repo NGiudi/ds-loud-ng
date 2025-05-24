ad9bac307c2d4429d4f67ed2db9c2bb5904bfd67
NGiudiDev <ngiudice.dev@gmail.com>
2025-05-24 14:14:47 -0300

    Updates

---
+++ assets/Colors.stories-4MR9GuwJ.js
@@ -0,0 +1,183 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{q as T,r as $,T as i,b as t}from"./index-DyPWuN9l.js";const B=$("div")`
+  ${s=>T`
+    align-items: center;
+    background-color: ${s.theme.colors[s.$color][s.$intensity]};
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 24px;
+    margin: 20px auto;
+    width: 90%;
+  `}
+`,r={ColorBox:B},S={title:"Theme/Colors"},l=()=>e.jsxs(i,{children:[e.jsxs(r.ColorBox,{$color:"black",$intensity:"_900",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_900"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#18181B"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_800",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_800"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#262626"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_700",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_700"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#363636"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_600",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_600"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#52525B"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_500",children:[e.jsx(t,{type:"subtitle",children:"_500"}),e.jsx(t,{type:"subtitle",children:"#71717A"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_400",children:[e.jsx(t,{type:"subtitle",children:"_400"}),e.jsx(t,{type:"subtitle",children:"#A1A1AA"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_300",children:[e.jsx(t,{type:"subtitle",children:"_300"}),e.jsx(t,{type:"subtitle",children:"#D4D4D8"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_200",children:[e.jsx(t,{type:"subtitle",children:"_200"}),e.jsx(t,{type:"subtitle",children:"#E4E4E7"})]}),e.jsxs(r.ColorBox,{$color:"black",$intensity:"_100",children:[e.jsx(t,{type:"subtitle",children:"_100"}),e.jsx(t,{type:"subtitle",children:"#F4F4F5"})]})]}),o=()=>e.jsxs(i,{children:[e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_900",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_900"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#7B441E"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_800",children:[e.jsx(t,{type:"subtitle",children:"_800"}),e.jsx(t,{type:"subtitle",children:"#B75404"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_700",children:[e.jsx(t,{type:"subtitle",children:"_700"}),e.jsx(t,{type:"subtitle",children:"#CE712A"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_600",children:[e.jsx(t,{type:"subtitle",children:"_600"}),e.jsx(t,{type:"subtitle",children:"#F47A1D"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_500",children:[e.jsx(t,{type:"subtitle",children:"_500"}),e.jsx(t,{type:"subtitle",children:"#F78223"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_400",children:[e.jsx(t,{type:"subtitle",children:"_400"}),e.jsx(t,{type:"subtitle",children:"#F79A51"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_300",children:[e.jsx(t,{type:"subtitle",children:"_300"}),e.jsx(t,{type:"subtitle",children:"#FAAD82"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_200",children:[e.jsx(t,{type:"subtitle",children:"_200"}),e.jsx(t,{type:"subtitle",children:"#FBBD89"})]}),e.jsxs(r.ColorBox,{$color:"orange",$intensity:"_100",children:[e.jsx(t,{type:"subtitle",children:"_100"}),e.jsx(t,{type:"subtitle",children:"#FEE4CD"})]})]}),n=()=>e.jsxs(i,{children:[e.jsxs(r.ColorBox,{$color:"red",$intensity:"_900",children:[e.jsx(t,{color:"black_100",type:"subtitle",children:"_900"}),e.jsx(t,{color:"black_100",type:"subtitle",children:"#7B1E1E"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_800",children:[e.jsx(t,{type:"subtitle",children:"_800"}),e.jsx(t,{type:"subtitle",children:"#B70404"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_700",children:[e.jsx(t,{type:"subtitle",children:"_700"}),e.jsx(t,{type:"subtitle",children:"#CE2A2A"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_600",children:[e.jsx(t,{type:"subtitle",children:"_600"}),e.jsx(t,{type:"subtitle",children:"#F41D1D"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_500",children:[e.jsx(t,{type:"subtitle",children:"_500"}),e.jsx(t,{type:"subtitle",children:"#F72323"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_400",children:[e.jsx(t,{type:"subtitle",children:"_400"}),e.jsx(t,{type:"subtitle",children:"#F75151"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_300",children:[e.jsx(t,{type:"subtitle",children:"_300"}),e.jsx(t,{type:"subtitle",children:"#FA8282"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_200",children:[e.jsx(t,{type:"subtitle",children:"_200"}),e.jsx(t,{type:"subtitle",children:"#FB8989"})]}),e.jsxs(r.ColorBox,{$color:"red",$intensity:"_100",children:[e.jsx(t,{type:"subtitle",children:"_100"}),e.jsx(t,{type:"subtitle",children:"#FECDCD"})]})]});l.__docgenInfo={description:"",methods:[],displayName:"BlackPallete"};o.__docgenInfo={description:"",methods:[],displayName:"OrangePallete"};n.__docgenInfo={description:"",methods:[],displayName:"RedPallete"};var x,c,y;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <Styles.ColorBox $color="black" $intensity="_900">\r
+        <Text color="black_100" type="subtitle">\r
+          _900\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #18181B\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_800">\r
+        <Text color="black_100" type="subtitle">\r
+          _800\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #262626\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_700">\r
+        <Text color="black_100" type="subtitle">\r
+          _700\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #363636\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_600">\r
+        <Text color="black_100" type="subtitle">\r
+          _600\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #52525B\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_500">\r
+        <Text type="subtitle">_500</Text>\r
+        <Text type="subtitle">#71717A</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_400">\r
+        <Text type="subtitle">_400</Text>\r
+        <Text type="subtitle">#A1A1AA</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_300">\r
+        <Text type="subtitle">_300</Text>\r
+        <Text type="subtitle">#D4D4D8</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_200">\r
+        <Text type="subtitle">_200</Text>\r
+        <Text type="subtitle">#E4E4E7</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="black" $intensity="_100">\r
+        <Text type="subtitle">_100</Text>\r
+        <Text type="subtitle">#F4F4F5</Text>\r
+      </Styles.ColorBox>\r
+    </ThemeDS>;
+}`,...(y=(c=l.parameters)==null?void 0:c.docs)==null?void 0:y.source}}};var b,_,d;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <Styles.ColorBox $color="orange" $intensity="_900">\r
+        <Text color="black_100" type="subtitle">\r
+          _900\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #7B441E\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_800">\r
+        <Text type="subtitle">_800</Text>\r
+        <Text type="subtitle">#B75404</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_700">\r
+        <Text type="subtitle">_700</Text>\r
+        <Text type="subtitle">#CE712A</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_600">\r
+        <Text type="subtitle">_600</Text>\r
+        <Text type="subtitle">#F47A1D</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_500">\r
+        <Text type="subtitle">_500</Text>\r
+        <Text type="subtitle">#F78223</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_400">\r
+        <Text type="subtitle">_400</Text>\r
+        <Text type="subtitle">#F79A51</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_300">\r
+        <Text type="subtitle">_300</Text>\r
+        <Text type="subtitle">#FAAD82</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_200">\r
+        <Text type="subtitle">_200</Text>\r
+        <Text type="subtitle">#FBBD89</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="orange" $intensity="_100">\r
+        <Text type="subtitle">_100</Text>\r
+        <Text type="subtitle">#FEE4CD</Text>\r
+      </Styles.ColorBox>\r
+    </ThemeDS>;
+}`,...(d=(_=o.parameters)==null?void 0:_.docs)==null?void 0:d.source}}};var p,u,a;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <Styles.ColorBox $color="red" $intensity="_900">\r
+        <Text color="black_100" type="subtitle">\r
+          _900\r
+        </Text>\r
+\r
+        <Text color="black_100" type="subtitle">\r
+          #7B1E1E\r
+        </Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_800">\r
+        <Text type="subtitle">_800</Text>\r
+        <Text type="subtitle">#B70404</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_700">\r
+        <Text type="subtitle">_700</Text>\r
+        <Text type="subtitle">#CE2A2A</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_600">\r
+        <Text type="subtitle">_600</Text>\r
+        <Text type="subtitle">#F41D1D</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_500">\r
+        <Text type="subtitle">_500</Text>\r
+        <Text type="subtitle">#F72323</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_400">\r
+        <Text type="subtitle">_400</Text>\r
+        <Text type="subtitle">#F75151</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_300">\r
+        <Text type="subtitle">_300</Text>\r
+        <Text type="subtitle">#FA8282</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_200">\r
+        <Text type="subtitle">_200</Text>\r
+        <Text type="subtitle">#FB8989</Text>\r
+      </Styles.ColorBox>\r
+\r
+      <Styles.ColorBox $color="red" $intensity="_100">\r
+        <Text type="subtitle">_100</Text>\r
+        <Text type="subtitle">#FECDCD</Text>\r
+      </Styles.ColorBox>\r
+    </ThemeDS>;
+}`,...(a=(u=n.parameters)==null?void 0:u.docs)==null?void 0:a.source}}};const k=["BlackPallete","OrangePallete","RedPallete"];export{l as BlackPallete,o as OrangePallete,n as RedPallete,k as __namedExportsOrder,S as default};
