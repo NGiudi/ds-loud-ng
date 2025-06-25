0c5d03e1bf12fd8b67bb81008c68bf6b86b7c503
NGiudiDev <ngiudice.dev@gmail.com>
2025-06-25 19:50:13 -0300

    Updates

--- assets/Tabs.stories-q1G6uEXD.js
+++
@@ -1,9 +0,0 @@
-import{j as a}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{f as o,T as m,g as n}from"./index-CniwK52R.js";const l={argTypes:{children:{control:{type:"none"},description:"Contenido de la tab"},margin:{control:{type:"text"},description:"Se setea la distancia de los margenes."},name:{control:{type:"text"},description:"Nombre que identifica el componente."},padding:{control:{type:"text"},description:"Se setea la distancia de los paddings."}},component:o,parameters:{status:{type:"beta"}},tags:["autodocs"],title:"Components/Tabs"},e=i=>a.jsx(m,{children:a.jsxs(o,{...i,children:[a.jsx(n,{name:"uno",children:"Página Uno"}),a.jsx(n,{name:"dos",children:"Página Dos"}),a.jsx(n,{name:"tres",children:"Página Tres"})]})});e.args={margin:"t-10 x-30"};e.__docgenInfo={description:"",methods:[],displayName:"DinamicExample"};var s,r,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`args => {
-  return <ThemeDS>\r
-      <Tabs {...args}>\r
-        <Tab name="uno">Página Uno</Tab>\r
-        <Tab name="dos">Página Dos</Tab>\r
-        <Tab name="tres">Página Tres</Tab>\r
-      </Tabs>\r
-    </ThemeDS>;
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const g=["DinamicExample"];export{e as DinamicExample,g as __namedExportsOrder,l as default};
---
+++ assets/Toast.stories-ZkIAosbr.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import{i as a,T as p}from"./index-BN2RcUVK.js";const c={argTypes:{children:{control:{type:"input"},description:"Mensaje que se mostrará en el toast."},type:{control:{type:"select"},description:"Se setea el tipo de toast.",options:["error","info","success"]}},component:a,parameters:{status:{type:"stable"}},tags:["autodocs"],title:"Components/Toast"},e=()=>t.jsx(p,{children:t.jsx(a,{margin:"b-32",type:"pageTitle",children:"Lorem Ipsum is simply dummy text"})});e.__docgenInfo={description:"",methods:[],displayName:"Types"};var s,r,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <ThemeDS>\r
+      <Toast margin="b-32" type="pageTitle">\r
+        Lorem Ipsum is simply dummy text\r
+      </Toast>\r
+    </ThemeDS>;
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const d=["Types"];export{e as Types,d as __namedExportsOrder,c as default};
