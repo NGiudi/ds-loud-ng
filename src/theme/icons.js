a49853120cd5ea8e22e8e7fef6e602a82c8c542d
NGiudiDev <ngiudice.dev@gmail.com>
2025-09-26 23:54:11 -0300

    update theme

---
+++ src/theme/icons.js
@@ -0,0 +1,301 @@
+import angleDownLight from "../assets/icons/angle-down-light.svg";
+import angleLeftLight from "../assets/icons/angle-left-light-full.svg";
+import angleRightLight from "../assets/icons/angle-right-light-full.svg";
+import angleUpLight from "../assets/icons/angle-up-light.svg";
+import arrowDownArrowUpLight from "../assets/icons/arrow-down-arrow-up-light.svg";
+import arrowDownLight from "../assets/icons/arrow-down-light.svg";
+import arrowDownToLineLight from "../assets/icons/arrow-down-to-line-light-full.svg";
+import arrowLeftLight from "../assets/icons/arrow-left-light-full.svg";
+import arrowLeftToLineLight from "../assets/icons/arrow-left-to-line-light-full.svg";
+import arrowRightArrowLeftLight from "../assets/icons/arrow-right-arrow-left-light-full.svg";
+import arrowRightFromBracketLight from "../assets/icons/arrow-right-from-bracket-light.svg";
+import arrowRightFromLineLight from "../assets/icons/arrow-right-from-line-light-full.svg";
+import arrowRightLight from "../assets/icons/arrow-right-light-full.svg";
+import arrowRightToBracketLight from "../assets/icons/arrow-right-to-bracket-light.svg";
+import arrowRotateLeftLight from "../assets/icons/arrow-rotate-left-light-full.svg";
+import arrowRotateRightLight from "../assets/icons/arrow-rotate-right-light-full.svg";
+import arrowUpFromLineLight from "../assets/icons/arrow-up-from-line-light-full.svg";
+import arrowUpLight from "../assets/icons/arrow-up-light.svg";
+import arrowUpRightAndArrowDownLeftFromCenterLight from "../assets/icons/arrow-up-right-and-arrow-down-left-from-center-light-full.svg";
+import arrowsLeftRightLight from "../assets/icons/arrows-left-right-light-full.svg";
+import arrowsRotateLight from "../assets/icons/arrows-rotate-light-full.svg";
+import arrowsUpDownLight from "../assets/icons/arrows-up-down-light-full.svg";
+import asteriskLight from "../assets/icons/asterisk-light.svg";
+import bagsShoppingLight from "../assets/icons/bags-shopping-light-full.svg";
+import banLight from "../assets/icons/ban-light.svg";
+import barsFilterLight from "../assets/icons/bars-filter-light.svg";
+import barsLight from "../assets/icons/bars-light-full.svg";
+import bellLight from "../assets/icons/bell-light-full.svg";
+import boxArchiveLight from "../assets/icons/box-archive-light.svg";
+import boxLight from "../assets/icons/box-light.svg";
+import boxOpenLight from "../assets/icons/box-open-light.svg";
+import boxTapedLight from "../assets/icons/box-taped-light-full.svg";
+import calendarCheckLight from "../assets/icons/calendar-check-light-full.svg";
+import calendarDayLight from "../assets/icons/calendar-day-light-full.svg";
+import cameraLight from "../assets/icons/camera-light.svg";
+import checkLight from "../assets/icons/check-light-full.svg";
+import checkRegular from "../assets/icons/check-regular-full.svg";
+import circleCheckLight from "../assets/icons/circle-check-light-full.svg";
+import circleDollarLight from "../assets/icons/circle-dollar-light.svg";
+import circleExclamationLight from "../assets/icons/circle-exclamation-light-full.svg";
+import circleInfoLight from "../assets/icons/circle-info-light-full.svg";
+import circleMinusLight from "../assets/icons/circle-minus-light.svg";
+import circlePauseLight from "../assets/icons/circle-pause-light.svg";
+import circlePlayLight from "../assets/icons/circle-play-light.svg";
+import circlePlusLight from "../assets/icons/circle-plus-light.svg";
+import circleQuestionLight from "../assets/icons/circle-question-light.svg";
+import circleXmarkLight from "../assets/icons/circle-xmark-light-full.svg";
+import clipboardLight from "../assets/icons/clipboard-light-full.svg";
+import clipboardListLight from "../assets/icons/clipboard-list-light-full.svg";
+import clockLight from "../assets/icons/clock-light.svg";
+import clockRotateLeftLight from "../assets/icons/clock-rotate-left-light.svg";
+import cloneLight from "../assets/icons/clone-light-full.svg";
+import commentLinesLight from "../assets/icons/comment-lines-light.svg";
+import creditCardLight from "../assets/icons/credit-card-light.svg";
+import cubeLight from "../assets/icons/cube-light-full.svg";
+import dollarSignLight from "../assets/icons/dollar-sign-light.svg";
+import dollyEmptyLight from "../assets/icons/dolly-empty-light-full.svg";
+import dollyLight from "../assets/icons/dolly-light-full.svg";
+import ellipsisSolid from "../assets/icons/ellipsis-solid-full.svg";
+import ellipsisVerticalLight from "../assets/icons/ellipsis-vertical-light-full.svg";
+import envelopeLight from "../assets/icons/envelope-light-full.svg";
+import envelopeOpenLight from "../assets/icons/envelope-open-light.svg";
+import expandLight from "../assets/icons/expand-light.svg";
+import eyeLight from "../assets/icons/eye-light.svg";
+import eyeSlashLight from "../assets/icons/eye-slash-light.svg";
+import faceFrownLight from "../assets/icons/face-frown-light.svg";
+import faceSmileLight from "../assets/icons/face-smile-light.svg";
+import fileLight from "../assets/icons/file-light.svg";
+import folderOpenLight from "../assets/icons/folder-open-light.svg";
+import forkliftLight from "../assets/icons/forklift-light.svg";
+import gearLight from "../assets/icons/gear-light-full.svg";
+import giftLight from "../assets/icons/gift-light-full.svg";
+import globeLight from "../assets/icons/globe-light.svg";
+import handHoldingBoxLight from "../assets/icons/hand-holding-box-light-full.svg";
+import headphonesLight from "../assets/icons/headphones-light.svg";
+import heartLight from "../assets/icons/heart-light.svg";
+import heartSolid from "../assets/icons/heart-solid.svg";
+import hourglassEndLight from "../assets/icons/hourglass-end-light.svg";
+import houseLight from "../assets/icons/house-light-full.svg";
+import imageLight from "../assets/icons/image-light.svg";
+import imagesLight from "../assets/icons/images-light.svg";
+import inboxLight from "../assets/icons/inbox-light.svg";
+import linkSimpleLight from "../assets/icons/link-simple-light.svg";
+import listCheckLight from "../assets/icons/list-check-light-full.svg";
+import locationCrosshairsLight from "../assets/icons/location-crosshairs-light-full.svg";
+import locationDotLight from "../assets/icons/location-dot-light-full.svg";
+import lockKeyholeLight from "../assets/icons/lock-keyhole-light.svg";
+import magnifyingGlassLight from "../assets/icons/magnifying-glass-light-full.svg";
+import mapLight from "../assets/icons/map-light-full.svg";
+import mapPinLight from "../assets/icons/map-pin-light-full.svg";
+import markerLight from "../assets/icons/marker-light.svg";
+import megaphoneLight from "../assets/icons/megaphone-light-full.svg";
+import microphoneLight from "../assets/icons/microphone-light.svg";
+import microphoneSlashLight from "../assets/icons/microphone-slash-light.svg";
+import minusLight from "../assets/icons/minus-light.svg";
+import paperPlaneLight from "../assets/icons/paper-plane-light.svg";
+import paperPlaneTopLight from "../assets/icons/paper-plane-top-light-full.svg";
+import paperclipVerticalLight from "../assets/icons/paperclip-vertical-light.svg";
+import penLight from "../assets/icons/pen-light-full.svg";
+import percentLight from "../assets/icons/percent-light.svg";
+import phoneHangupLight from "../assets/icons/phone-hangup-light.svg";
+import phoneLight from "../assets/icons/phone-light.svg";
+import plugLight from "../assets/icons/plug-light.svg";
+import plusLight from "../assets/icons/plus-light.svg";
+import printLight from "../assets/icons/print-light.svg";
+import puzzlePieceLight from "../assets/icons/puzzle-piece-light-full.svg";
+import qrcodeLight from "../assets/icons/qrcode-light-full.svg";
+import receiptLight from "../assets/icons/receipt-light.svg";
+import rectangleBarcodeLight from "../assets/icons/rectangle-barcode-light-full.svg";
+import rectangleHistoryCirclePlusLight from "../assets/icons/rectangle-history-circle-plus-light.svg";
+import scannerGunLight from "../assets/icons/scanner-gun-light.svg";
+import scissorsLight from "../assets/icons/scissors-light.svg";
+import shareNodesLight from "../assets/icons/share-nodes-light.svg";
+import shirtLight from "../assets/icons/shirt-light.svg";
+import shirtRunningLight from "../assets/icons/shirt-running-light-full.svg";
+import shirtTankTopLight from "../assets/icons/shirt-tank-top-light-full.svg";
+import shoppingBagLight from "../assets/icons/shopping-bag-light.svg";
+import spinnerThirdDuotone from "../assets/icons/spinner-third-duotone-solid-full.svg";
+import squareSuccessLight from "../assets/icons/square-check-light-full.svg";
+import squareMinusLight from "../assets/icons/square-minus-light-full.svg";
+import squareXmarkLight from "../assets/icons/square-xmark-light-full.svg";
+import starLight from "../assets/icons/star-light.svg";
+import starSolid from "../assets/icons/star-solid.svg";
+import storeLight from "../assets/icons/store-light.svg";
+import tagLight from "../assets/icons/tag-light.svg";
+import tagsLight from "../assets/icons/tags-light.svg";
+import thumbsDownLight from "../assets/icons/thumbs-down-light.svg";
+import thumbsUpLight from "../assets/icons/thumbs-up-light.svg";
+import thumbtackLight from "../assets/icons/thumbtack-light.svg";
+import toogleOnRegular from "../assets/icons/toggle-on-regular-full.svg";
+import trashLight from "../assets/icons/trash-light-full.svg";
+import triangleExclamationLight from "../assets/icons/triangle-exclamation-light-full.svg";
+import truckClockLight from "../assets/icons/truck-clock-light.svg";
+import truckFastLight from "../assets/icons/truck-fast-light.svg";
+import truckLight from "../assets/icons/truck-light.svg";
+import truckMovingLight from "../assets/icons/truck-moving-light.svg";
+import truckRampBoxLight from "../assets/icons/truck-ramp-box-light.svg";
+import truckRampCouchLight from "../assets/icons/truck-ramp-couch-light-full.svg";
+import truckRampLight from "../assets/icons/truck-ramp-light.svg";
+import userCheckLight from "../assets/icons/user-check-light-full.svg";
+import userGroupLight from "../assets/icons/user-group-light.svg";
+import userLargeLight from "../assets/icons/user-large-light.svg";
+import userSlashLight from "../assets/icons/user-slash-light-full.svg";
+import videoLight from "../assets/icons/video-light.svg";
+import warehouseLight from "../assets/icons/warehouse-light.svg";
+import weightHangingLight from "../assets/icons/weight-hanging-light-full.svg";
+import wineGlassCrackLight from "../assets/icons/wine-glass-crack-light-full.svg";
+import xmarkLight from "../assets/icons/xmark-light-full.svg";
+import xmarkToSlotLight from "../assets/icons/xmark-to-slot-light-full.svg";
+
+export const icons = {
+  active: puzzlePieceLight,
+  activeUser: userCheckLight,
+  archive: boxArchiveLight,
+  associated: rectangleHistoryCirclePlusLight,
+  attach: paperclipVerticalLight,
+  awaitingExchange: clockRotateLeftLight,
+  awaitingPayment: creditCardLight,
+  downAngle: angleDownLight,
+  backAngle: angleLeftLight,
+  backArrow: arrowLeftLight,
+  barcode: rectangleBarcodeLight,
+  box: boxLight,
+  boxOpen: boxOpenLight,
+  boxTaped: boxTapedLight,
+  burger: barsLight,
+  calendar: calendarDayLight,
+  camera: cameraLight,
+  cancel: circleXmarkLight,
+  cancelled: banLight,
+  caution: triangleExclamationLight,
+  change: arrowRightArrowLeftLight,
+  chat: commentLinesLight,
+  check: checkLight,
+  checkRegular: checkRegular,
+  checklist: listCheckLight,
+  circleMinus: circleMinusLight,
+  circlePlus: circlePlusLight,
+  close: xmarkLight,
+  connect: plugLight,
+  copy: cloneLight,
+  created: hourglassEndLight,
+  createdAt: calendarCheckLight,
+  cut: scissorsLight,
+  delete: trashLight,
+  delivered: thumbsUpLight,
+  downArrow: arrowDownLight,
+  download: arrowDownToLineLight,
+  edit: penLight,
+  ellipsis: ellipsisSolid,
+  email: envelopeLight,
+  emailOpen: envelopeOpenLight,
+  error: circleExclamationLight,
+  estimatedDelivery: clockLight,
+  expand: expandLight,
+  export: arrowRightFromLineLight,
+  failed: wineGlassCrackLight,
+  fatherProduct: bagsShoppingLight,
+  file: fileLight,
+  filter: barsFilterLight,
+  fix: thumbtackLight,
+  gift: giftLight,
+  globe: globeLight,
+  handOver: handHoldingBoxLight,
+  happy: faceSmileLight,
+  headphones: headphonesLight,
+  heartFull: heartSolid,
+  heartStroke: heartLight,
+  height: arrowsUpDownLight,
+  helpCenter: circleQuestionLight,
+  hide: eyeLight,
+  home: houseLight,
+  image: imageLight,
+  images: imagesLight,
+  import: arrowLeftToLineLight,
+  inactiveUser: userSlashLight,
+  inbox: inboxLight,
+  info: circleInfoLight,
+  inProgress: paperPlaneLight,
+  inventory: shirtLight,
+  itemAdd: dollyLight,
+  itemNull: dollyEmptyLight,
+  label: tagLight,
+  labeled: tagsLight,
+  large: arrowUpRightAndArrowDownLeftFromCenterLight,
+  link: linkSimpleLight,
+  listItems: clipboardListLight,
+  loading: truckRampCouchLight,
+  truck: truckRampBoxLight,
+  locationArrow: paperPlaneTopLight,
+  locationDot: locationDotLight,
+  lost: squareXmarkLight,
+  mapPage: mapLight,
+  mapPin: mapPinLight,
+  mark: markerLight,
+  microphone: microphoneLight,
+  microphoneSlash: microphoneSlashLight,
+  minus: minusLight,
+  money: circleDollarLight,
+  new: asteriskLight,
+  nextAngle: angleRightLight,
+  nextArrow: arrowRightLight,
+  notDelivered: thumbsDownLight,
+  notLabeled: xmarkToSlotLight,
+  notReenterVariant: shirtRunningLight,
+  notification: bellLight,
+  options: ellipsisVerticalLight,
+  orders: shoppingBagLight,
+  pause: circlePauseLight,
+  percentage: percentLight,
+  phone: phoneLight,
+  phoneHangup: phoneHangupLight,
+  pickupRequested: truckRampLight,
+  placement: locationCrosshairsLight,
+  play: circlePlayLight,
+  plus: plusLight,
+  price: dollarSignLight,
+  print: printLight,
+  process: forkliftLight,
+  profile: userLargeLight,
+  publication: megaphoneLight,
+  qr: qrcodeLight,
+  reenterVariant: shirtTankTopLight,
+  receipt: receiptLight,
+  recover: folderOpenLight,
+  redo: arrowRotateRightLight,
+  resume: clipboardLight,
+  sad: faceFrownLight,
+  scan: scannerGunLight,
+  search: magnifyingGlassLight,
+  security: lockKeyholeLight,
+  setting: gearLight,
+  share: shareNodesLight,
+  shipped: truckMovingLight,
+  shipment: truckLight,
+  shippingFast: truckFastLight,
+  shippingTime: truckClockLight,
+  signIn: arrowRightToBracketLight,
+  signOut: arrowRightFromBracketLight,
+  sort: arrowDownArrowUpLight,
+  spinner: spinnerThirdDuotone,
+  squareSuccess: squareSuccessLight,
+  starFull: starSolid,
+  starStroke: starLight,
+  store: storeLight,
+  success: circleCheckLight,
+  sync: arrowsRotateLight,
+  team: userGroupLight,
+  toggleOn: toogleOnRegular,
+  undo: arrowRotateLeftLight,
+  upAngle: angleUpLight,
+  upArrow: arrowUpLight,
+  upload: arrowUpFromLineLight,
+  video: videoLight,
+  view: eyeSlashLight,
+  volume: cubeLight,
+  warehouse: warehouseLight,
+  warning: squareMinusLight,
+  weight: weightHangingLight,
+  width: arrowsLeftRightLight,
+};
---
+++ src/theme/typography.js
@@ -0,0 +1,41 @@
+export const typography = {
+  body: {
+    regular: {
+      lineHeight: "20px",
+      size: "14px",
+      weight: 400,
+    },
+    semibold: {
+      lineHeight: "20px",
+      size: "14px",
+      weight: 600,
+    },
+  },
+  caption: {
+    regular: {
+      lineHeight: "18px",
+      size: "12px",
+      weight: 400,
+    },
+    semibold: {
+      lineHeight: "18px",
+      size: "12px",
+      weight: 600,
+    },
+  },
+  pageTitle: {
+    lineHeight: "42px",
+    size: "28px",
+    weight: 600,
+  },
+  subtitle: {
+    lineHeight: "28px",
+    size: "18px",
+    weight: 600,
+  },
+  title: {
+    lineHeight: "32px",
+    size: "22px",
+    weight: 800,
+  },
+};
